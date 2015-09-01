f434488671232c96361e135ecb2237f00b2838eb
railsfactory-vivek <vivek@railsfactorys-MacBook-Pro.local>
2015-09-01 12:23:45 +0530

    Loop through values can be done for half of the value as normally square root of the value is used for finding the largest prime number which wouldn't give largest prime factor

--- find_prime_numbers.js
+++ find_prime_numbers.js
@@ -1,10 +1,9 @@
-/* Program 3: Find the largest prime factor for 600851475143 */
 let lodash=require('lodash');
-let prime = [1], max =600851475143, k=1;
+let prime = [1], max = 600851475143, k=1;
 function find_prime(max) {
-		for(let i=2; i < max; i++) {
-		let j=2;
-		for(j=2; j<i; j++) {
+		for(let i=2; i < max/2; i++) {
+		let j=2
+		for(; j<i; j++) {
 				if(i % j == 0) {
 						break
 			 }	
